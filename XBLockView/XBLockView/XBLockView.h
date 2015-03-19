3c8026a007c39f673dd467bcf495be6a0068f73b
king824 <i@jxb.name>
2015-03-19 14:44:22 +0800

    first commit

---
+++ XBLockView/XBLockView/XBLockView.h
@@ -0,0 +1,13 @@
+//
+//  XBLockView.h
+//  XBLockView
+//
+//  Created by Peter on 15/3/19.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XBLockView : UIView
+
+@end
---
+++ XBLockView/XBLockView/XBLockView.m
@@ -0,0 +1,13 @@
+//
+//  XBLockView.m
+//  XBLockView
+//
+//  Created by Peter on 15/3/19.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import "XBLockView.h"
+
+@implementation XBLockView
+
+@end
